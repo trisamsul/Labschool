bd9761b9e519be00cdf8b4a73ba8914cc4c2e46a
Tri Samsul R <trisamsul@gmail.com>
2017-11-04 15:16:59 +0700

    Initial Commit

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,86 @@
+
+<footer class="footer-v1">
+  <div class="menuFooter clearfix">
+    <div class="container">
+      <div class="row clearfix">
+
+        <div class="col-sm-3 col-xs-6">
+		  <h5>Link Terkait:</h5>
+          <ul class="menuLink">
+            <li><a href="http://upi.edu">Universitas Pendidikan Indonesia</a></li>
+            <li><a href="http://disdik.bandung.go.id/">Dinas Pendidikan Kota Bandung</a></li>
+            <li><a href="http://dapo.dikdasmen.kemdikbud.go.id/">Data Pokok Pendidikan</a></li>
+            <li><a href="http://ppdb.bandung.go.id/portal/">PPDB Kota Bandung</a></li>
+          </ul>
+        </div><!-- col-sm-3 col-xs-6 -->
+
+        <div class="col-sm-3 col-xs-6 borderLeft">
+		   <h5>Download:</h5>
+          <ul class="menuLink">
+            <li><a href="http://bse.kemdikbud.go.id/">Buku Sekolah Elektronik</a></li>
+            <li><a href="http://bos.kemdikbud.go.id/">Pedoman Pelaporan BOS</a></li>
+            <li><a href="http://bansm.or.id/">Pedoman Akreditasi Sekolah</a></li>
+          </ul>
+        </div><!-- col-sm-3 col-xs-6 -->
+
+        <div class="col-sm-3 col-xs-6 borderLeft">
+          <div class="footer-address">
+            <h5>Lokasi:</h5>
+            <address>
+              Kampus Bumi Siliwangi<br>
+              Jl. Dr. Setiabuhi No. 229<br>
+              Bandung, Jabar, 40154
+            </address>
+            <a href="#"><span class="place"><i class="fa fa-map-marker"></i>Kampus Utama</span></a>
+          </div>
+        </div><!-- col-sm-3 col-xs-6 -->
+
+        <div class="col-sm-3 col-xs-6 borderLeft">
+          <div class="contactNo">
+            <h5>Telepon:</h5>
+            <h3><i class="fa fa-phone"></i> 022-2013342</h3>
+          </div><!-- contactNo -->
+          <br>
+          <div class="contactNo">
+            <h5>Fax:</h5>
+            <h3><i class="fa fa-fax"></i> 022-2013342</h3>
+          </div><!-- contactNo -->
+        </div><!-- col-sm-3 col-xs-6 -->
+
+      </div><!-- row -->
+    </div><!-- container -->
+  </div><!-- menuFooter -->
+
+  <div class="footer clearfix">
+    <div class="container">
+      <div class="row clearfix">
+        <div class="col-sm-12 col-xs-12 copyRight">
+          <center><p>© 2017 Copyright Sekolah Laboratorium UPI, Developed by <a href="#">Inquery</a></p></center>
+        </div><!-- col-sm-6 col-xs-12 -->
+      </div><!-- row clearfix -->
+    </div><!-- container -->
+  </div><!-- footer -->
+</footer>
+
+</div>
+
+<!-- JQUERY SCRIPTS -->
+<script src="<?php echo base_url(); ?>plugins/jquery/jquery-1.11.1.min.js"></script>
+<script src="<?php echo base_url(); ?>plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>plugins/flexslider/jquery.flexslider.js"></script>
+<script src="<?php echo base_url(); ?>plugins/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<script src="<?php echo base_url(); ?>plugins/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script src="<?php echo base_url(); ?>plugins/selectbox/jquery.selectbox-0.1.3.min.js"></script>
+<script src="<?php echo base_url(); ?>plugins/pop-up/jquery.magnific-popup.js"></script>
+<script src="<?php echo base_url(); ?>plugins/animation/waypoints.min.js"></script>
+<script src="<?php echo base_url(); ?>plugins/count-up/jquery.counterup.js"></script>
+<script src="<?php echo base_url(); ?>plugins/animation/wow.min.js"></script>
+<script src="<?php echo base_url(); ?>plugins/animation/moment.min.js"></script>
+<script src="<?php echo base_url(); ?>plugins/calender/fullcalendar.min.js"></script>
+<script src="<?php echo base_url(); ?>plugins/owl-carousel/owl.carousel.js"></script>
+<script src="<?php echo base_url(); ?>plugins/timer/jquery.syotimer.js"></script>
+<script src="<?php echo base_url(); ?>plugins/smoothscroll/SmoothScroll.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/custom.js"></script>
+
+</body>
+</html>
---
+++ application/views/layouts/header.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sekolah Laboratorium - UPI</title>
+
+  <!-- PLUGINS CSS STYLE -->
+  <link rel="icon" type="image/png" href="img/favicon.png">
+  <link href="<?php echo base_url(); ?>plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/selectbox/select_option1.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/flexslider/flexslider.css" type="text/css" media="screen" />
+  <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/calender/fullcalendar.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/animate.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/pop-up/magnific-popup.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>plugins/rs-plugin/css/settings.css" media="screen">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>plugins/owl-carousel/owl.carousel.css" media="screen">
+
+  <!-- GOOGLE FONT -->
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,600italic,400italic,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+
+  <!-- CUSTOM CSS -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/default.css" id="option_color">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="body-wrapper">
+
+  <div class="main_wrapper">
+
+    <header class="header-wrapper">
+     
+
+      <div class="header clearfix" style="background-color: #C10E0E;">
+        <nav class="navbar navbar-main navbar-default">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12">
+                <div class="header_inner">
+
+                  <!-- Brand and toggle get grouped for better mobile display -->
+                  <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand logo clearfix" href="index.html"><img src="img/logo1.png" alt="" class="img-responsive" /></a>
+                  </div>
+
+                  <!-- Collect the nav links, forms, and other content for toggling -->
+                  <div class="collapse navbar-collapse" id="main-nav">
+                    <ul class="nav navbar-nav navbar-right">
+                      <li class="active">
+                        <a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Beranda</a>
+                      </li>
+					  <li class=" dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profil</a>
+                        <ul class="dropdown-menu">
+                          <li><a href="#">Sejarah</a></li>
+                          <li><a href="#">Visi & Misi</a></li>
+                          <li><a href="#">Pengelola Sekolah</a></li>
+                          <li><a href="#">Pimpinan Sekolah</a></li>
+                        </ul>
+                      </li>
+					  <li class=" dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Jenjang Sekolah</a>
+                        <ul class="dropdown-menu">
+                          <li class=" dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">TK</a>
+                              <ul class="dropdown-menu">
+                                <li><a href="http://tk.labschool.upi.edu/">TK Bumi Siliwangi</a></li>
+                                <li><a href="#">TK Kampus Cibiru</a></li>
+                              </ul>
+                            </li>
+                          <li class=" dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SD</a>
+                              <ul class="dropdown-menu">
+                                <li><a href="http://sd.labschool.upi.edu/">SD Bumi Siliwangi</a></li>
+                                <li><a href="http://sdlabschoolupicibiru.com/">SD Kampus Cibiru</a></li>
+                                <li><a href="http://www.sdlab-upitasik.sch.id/">SD Kampus Tasikmalaya</a></li>
+                                <li><a href="http://sdlab-upiserang.blogspot.co.id/">SD Kampus Serang</a></li>
+                              </ul>
+                            </li>
+                          <li class=" dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SMP</a>
+                            <ul class="dropdown-menu">
+                              <li><a href="http://smp.labschool.upi.edu/">SMP Bumi Siliwangi</a></li>
+                              <li><a href="http://smplabupicbr.mysch.id/">SMP Kampus Cibiru</a></li>
+                            </ul>
+                          </li>
+                          <li class=" dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SMA</a>
+                            <ul class="dropdown-menu">
+                              <li><a href="http://www.smalabupi.sch.id/">SMA Bumi Siliwangi</a></li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                      <li class="">
+                          <a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Riset</a>
+                        </li>
+                      <li class="">
+                          <a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Galeri</a>
+                        </li>
+                      <li class="">
+                          <a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Kontak</a>
+                        </li>
+                    </ul>
+                  </div><!-- navbar-collapse -->
+
+                </div>
+              </div>
+            </div>
+          </div><!-- /.container -->
+        </nav><!-- navbar -->
+      </div>
+    </header>
